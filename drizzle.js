e6c59cdff987d118d92e81d3eb9d8debc711506c
cyber-coderx <obanryebb@gmail.com>
2024-07-23 15:57:50 +0000

    Create drizzle.js

---
+++ drizzle.js
@@ -0,0 +1,32 @@
+import { sql } from "@vercel/postgres";
+import { drizzle } from 'drizzle-orm/vercel-postgres';
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uniqueIndex,
+} from 'drizzle-orm/pg-core';
+
+export const db = drizzle(sql);
+
+export const ExampleTable = pgTable(
+  'users',
+  {
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
+    email: text('email').notNull(),
+    image: text('image').notNull(),
+    createdAt: timestamp('createdAt').defaultNow().notNull(),
+  },
+  (users) => {
+    return {
+      uniqueIdx: uniqueIndex('unique_idx').on(users.email),
+    };
+  },
+);
+
+export const getExampleTable = async () => {
+  const selectResult = await db.select().from(ExampleTable);
+  console.log('Results', selectResult);
+};
